baac3c0e7f9167ab55917cf96886ab6cbff9c96d
Kosi Nwabueze <kosinwabueze@gmail.com>
2021-02-14 20:05:12 -0800

    hotfix(cors): removed localhost from cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const socket = require("socket.io");
 const ss = require("socket.io-stream")
 const io = socket(server, {
     cors: {
-        origin: ["http://localhost:3000", "https://cfe.house/"],
+        origin: process.env.HOST || "https://cfe.house/",
     }
 });
 
